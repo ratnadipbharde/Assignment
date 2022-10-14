6b039d135dccf2c45757e427ca7ce3ab04e77836
ratnadipbharde <ratnadipbharde@gmail.com>
2022-10-14 13:59:18 +0530

    [ratnadip]commit to correction

--- day5prob02/3/que4.sh
+++ day5prob02/3/que4.sh
@@ -5,22 +5,25 @@ Press 3 to convert (Feet to Meter)
 Press 4 to convert (Meter to Feet) "
 read n
 case $n in
-        1) echo -n "Enter feet"
+        1) echo -n "Enter feet :-"
         read feet
          inch=$(( $feet * 12 ))
          echo "$feet feet = $inch inch"
         ;;
-        2) echo -n "Enter inch"
+        2) echo -n "Enter inch :-"
         read inch
          feet=$(( $inch / 12 ))
          echo "$inch inch = $feet feet" 
         ;;
-        3) echo -n "Enter feet"
+        3) echo -n "Enter feet :-"
         read feet
         meter=$(echo  $feet 0.3048 | awk '{ print $1*$2}')
         echo "$feet feet = $meter meter"
         ;;
-        4) echo -n "Enter Meter " 
+        4) echo -n "Enter Meter :-"
+        read meter
+        feet=$(echo  $meter 3.2808399 | awk '{ print $1*$2}')
+        echo "$meter meter = $feet feet"
         ;;
     
     esac
